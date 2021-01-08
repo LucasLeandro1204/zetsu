7a444b0c80c8e9ae75f7a5d59ac86865bf9eb5d9
LucasLeandro1204 <lucasleandro1204@gmail.com>
2021-01-07 21:28:15 -0300

    chore: wip

---
+++ packages/app-components/src/components/Icon.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    name: String,
+
+    size: {
+      type: [String, Number],
+      default: 24,
+    },
+  },
+};
+</script>
+
+<template>
+  <svg
+    :height="size"
+    :width="size"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    class="z-icon inline-block"
+  >
+    <path :d="icon"></path>
+  </svg>
+</template>
+
--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,3 +1,13 @@
+<script>
+import { Icon } from '@zetsu/app-components';
+
+export default {
+  components: {
+    Icon,
+  },
+};
+</script>
+
 <template>
-  <p>APP</p>
+  <p>APP <Icon name="foo" /> </p>
 </template>
