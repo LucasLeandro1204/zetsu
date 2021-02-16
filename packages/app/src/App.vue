7499cfcbc812ba4f103a9024240b1fe1a86f1f4e
LucasLeandro1204 <lucasleandro1204@gmail.com>
2021-02-16 19:31:47 -0300

    chore: wip

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -12,14 +12,6 @@
 <div
   class="w-full h-full flex-1 flex bg-gradient-to-tr from-darkest to-darker"
 >
-  <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <pattern id="grid" x="-30" width="32px" height="32px" patternUnits="userSpaceOnUse">
-      <rect width="100%" height="100%" stroke="currentColor" stroke-width="1px"/>
-    </pattern>
-  </defs>
-
-  <rect width="100%" height="100%" fill="url(#grid)" />
-</svg>
+<iframe>aef</iframe>
 </div>
 </template>
---
+++ packages/app/src/components/GridBackground.vue
@@ -0,0 +1,11 @@
+<template>
+<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <pattern id="grid" x="-30" width="32px" height="32px" patternUnits="userSpaceOnUse">
+      <rect width="100%" height="100%" stroke="currentColor" stroke-width="1px"/>
+    </pattern>
+  </defs>
+
+  <rect width="100%" height="100%" fill="url(#grid)" />
+</svg>
+</template>
